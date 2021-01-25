8a350bfdee9f514cc4cf05be6204ff0be20ba4dc
neiloxx <anastasia.v.sido@gmail.com>
2021-01-25 16:14:44 +0600

    feat: add check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let strings = bracketsConfig.map(bracket => bracket.join(''));
+    let strLength = str.length;
+    let strNewLength = -1;
+    while (strLength !== strNewLength) {
+        strLength = str.length;
+        for (let i = 0; i < strings.length; i++) {
+            str = str.replaceAll(strings[i], '');
+        }
+        strNewLength = str.length;
+    }
+    return str.length === 0;
 }
